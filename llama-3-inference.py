8dc918e6ff97879e31e03d7dd97bdae86a50515a
Dinesh Jackson Samuel <jacksoncse@gmail.com>
2024-05-06 11:32:55 -0500

    Update llama-3-inference.py

--- llama-3-inference.py
+++ llama-3-inference.py
@@ -17,9 +17,11 @@ st.write("<h3 style='text-align:center'>This app uses a pre-trained Llama model 
 user_input = st.text_input("Enter your message:", key="user_input", placeholder="Type your message here...")
 if user_input:
     model_path = "meta-llama/Meta-Llama-3-8B-Instruct"
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
-
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto")
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
     response = model.generate(input_ids, max_length=2500, temperature=0.3, num_return_sequences=1)
     response_text = tokenizer.decode(response[0], skip_special_tokens=True)
